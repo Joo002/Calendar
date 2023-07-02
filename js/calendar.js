1218c7f6c4cf4002ca7331b143e77b4c0c943c64
Joo002 <83010631+Joo002@users.noreply.github.com>
2023-07-02 17:11:31 +0900

    Add files via upload

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,7 @@
 thisMonth = new Date();
 thisMonth.setDate(1);
 selectedDate = new Date();
+
 function calendar_update(){
     dates = []
     firstdatesLine = []
@@ -18,16 +19,15 @@ function calendar_update(){
         ele_date.className = "date";
         ele_date.id = i
         ele_date.style.cursor = "pointer";
-        ele_date.addEventListener("click", (e) => {selectedDate = dates[e.target.id]; calendar_update();})
         ele_date_.innerText = today.getDate();
-        ele_date__.innerText = "　";
+        ele_date__.innerText = "900,000,000\\";
         if (thisMonth.getMonth() != today.getMonth()) { ele_date.classList.add("otherMonth") }
         if (selectedDate.getMonth() == today.getMonth() && selectedDate.getDate() == today.getDate()) { ele_date.classList.add("selected") }
-        if (today.getDate() == 1) { ele_date.style.scrollSnapAlign = "start"; firstdatesLine.push(parseInt(i/7))}
+        if (today.getDate() == 1) { firstdatesLine.push(parseInt(i/7))}
         document.querySelector(".dates").append(ele_date);                
     }
     firstdatesLine = firstdatesLine.slice(0,3)
-    document.querySelector(".dates").scrollTo(0,100*firstdatesLine[1])
+    //document.querySelector(".dates").scrollTo(0,100*firstdatesLine[1])
 }
 function calendar_update_Month(){
     document.querySelector(".month").value = thisMonth.getFullYear() + "-" + String((thisMonth.getMonth() + 1)).padStart(2,'0');
@@ -40,28 +40,49 @@ function calendar_update_Month(){
     
 }
 
+drag_mode = 0;
+
+
 calendar_update();
+document.querySelector(".dates").scrollTo(0,500)
+document.querySelector(".dates").addEventListener("mousedown", e => {
+    clicked_scrollY = document.querySelector(".dates").scrollTop;
+    clicked_mouseY = e.clientY;
 
-changed = false;
 
-document.querySelector(".dates").addEventListener("scroll", e => {
-    console.log(document.querySelector(".dates").scrollTop);
-    if (document.querySelector(".dates").scrollTop < 300 && changed == false){
-        thisMonth.setMonth(thisMonth.getMonth() - 1)
-        calendar_update_Month();
-        changed = true;
+    temp = mouseY;
+    window.setTimeout(function(){
+        if(temp == mouseY){
+            selectedDate = dates[e.target.id]; calendar_update();
+            drag_mode = 2;
+        } else {
+            drag_mode = 1;
+        }
+    }, 50)
+})
+
+window.addEventListener("mousemove", e => {
+    mouseY = e.pageY;
+    if(drag_mode == 1 && e.buttons == 1){
+        if (document.querySelector(".dates").scrollTop < ((firstdatesLine[0] + firstdatesLine[1])/2 * 100)){
+            clicked_scrollY += ((firstdatesLine[1] - firstdatesLine[0]) * 100);
+            thisMonth.setMonth(thisMonth.getMonth() - 1)
+            calendar_update();
+        }
+        if (document.querySelector(".dates").scrollTop > ((firstdatesLine[1] + firstdatesLine[2])/2 * 100)){
+            clicked_scrollY += ((firstdatesLine[1] - firstdatesLine[2]) * 100);
+            thisMonth.setMonth(thisMonth.getMonth() + 1)
+            calendar_update();
+        }
+        document.querySelector(".dates").scrollTo(0, clicked_scrollY + clicked_mouseY - e.clientY)
     }
-    if (document.querySelector(".dates").scrollTop > 700 && changed == false){
-        thisMonth.setMonth(thisMonth.getMonth() + 1)
-        calendar_update_Month();
-        changed = true;
+    if(drag_mode == 2 && e.buttons == 1){
+        if(e.target.classList.contains("date") && !(e.target.classList.contains("otherMonth"))){
+            selectedDate = dates[e.target.id]; calendar_update();
+        }
     }
-    if (document.querySelector(".dates").scrollTop <= (firstdatesLine[0] * 100 + 1)){
-        calendar_update();
-        changed = false;
-    }
-    if (document.querySelector(".dates").scrollTop >= (firstdatesLine[2] * 100 - 1)){
-        calendar_update();
-        changed = false;
-    }
+})
+window.addEventListener("mouseup", e => {
+    document.querySelector(".dates").scrollTo({top:(firstdatesLine[1] * 100), left:0, behavior:'smooth'})
+    drag_mode = false;
 })
